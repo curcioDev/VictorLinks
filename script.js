bd5d0017d3048661a74e93d0112cf6ba279b8ed2
Victor <curciovictor1@gmail.com>
2025-07-28 16:42:39 -0300

    quase pronto

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ function toggleMode() {
     img.setAttribute("src", "./assets/avatar.png")
   }
 }
+
+function mostrarEmail(event) {
+  event.preventDefault()
+
+  const texto = document.getElementById("email-texto")
+  const email = "vcurcio.dev@gmail.com"
+  const original = "Email Profissional"
+
+  texto.classList.remove("fade-in")
+  texto.classList.add("fade-out")
+
+  setTimeout(() => {
+    texto.textContent = texto.textContent === original ? email : original
+    texto.classList.remove("fade-out")
+    texto.classList.add("fade-in")
+  }, 300) // tempo igual ao da animação
+}
